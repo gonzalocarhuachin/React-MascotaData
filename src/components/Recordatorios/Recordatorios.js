e96c17e2404e46a6df0ea628b74cc23f85ca38aa
gonzalocarhuachin <lgonzalocarhuachin@gmail.com>
2020-12-07 12:13:38 -0500

    Tabla detalle

--- src/components/Recordatorios/Recordatorios.js
+++ src/components/Recordatorios/Recordatorios.js
@@ -1,17 +1,87 @@
 import React, { Component } from 'react';
+import { ApiWebUrl } from '../utils';
 import './Recordatorios.css';
 
 export default class Recordatorios extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            listaMascotas:[],
+            mascotaSeleccionada : ""
+        }
+    }
+
+    componentDidMount(){
+        const rutaServicio = ApiWebUrl + "mascotas.php";
+        fetch(rutaServicio)
+        .then(
+            res => res.json()
+        )
+        .then(
+            (result) => {
+                console.log(result);
+                this.setState({
+                    listaMascotas:result
+                })
+            }
+        )
+    }
+
+    dibujarMascotas = (datosTablaMascotas) => {
+        return(
+            <ul className="list-group lista-mascotas-recordatorios">
+            {datosTablaMascotas.map(itemMascota=>
+                <li className="list-group-item" 
+                    id={"tr-mascota-"+itemMascota.idmascota}
+                    key={itemMascota.idmascota}
+                    onClick={() => this.seleccionarMascota(itemMascota)}>
+                        <h5 id="titulo-masc-record">{itemMascota.nombre}</h5>
+                        <small id="subtitulo-masc-record">{itemMascota.raza}</small>
+                </li>
+            )}   
+            </ul>
+        )
+    }
+    seleccionarMascota = (itemMascota) => {
+
+        if(this.state.mascotaSeleccionada !== ""){
+            document.getElementById("tr-mascota-" + this.state.mascotaSeleccionada.idmascota).classList.remove("fila-seleccionada")
+          }  
+
+        this.setState({ mascotaSeleccionada : itemMascota })
+
+        document.getElementById("tr-mascota-" + itemMascota.idmascota).classList.add("fila-seleccionada")
+    }
+
     render (){
+        let contenidoMascotas = this.dibujarMascotas(this.state.listaMascotas);
+        let dibujarComponenteRecordatorios = <Recordatorios mascotaRecordatorio={this.state.mascotaSeleccionada}/>
         return (
             <div className="container-fluid cajafuerarecordatorios">
                 <div className="container cajadentrorecordatorios">
                     <div className="row filarecordatorios">
                         <div className="col-md-3">
-                            <h2 className="titulo-recordatorios">Recordatorios</h2>
-                            <ul className="list-group">
-                                
-                            </ul>
+                            <h2 id="titulo-recordatorio" className="titulo-recordatorios">Recordatorios</h2>
+                            {contenidoMascotas}
+                        </div>
+                        <div className="col-md-9">
+                            <table className="table table-bordered">
+                                <thead className="thead-dark">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Descripción</th>
+                                        <th>Fecha</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
